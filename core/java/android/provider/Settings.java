39dbe48ba36827e55f24c2ec38a03fba36c9402d
maxwen <max.weninger@gmail.com>
2021-11-06 20:40:52 +0100

    SystemUI: add enablement settings for OmniJaws keyguard slice provider
    Change-Id: Ic3a386fae8ca63708628cd0b5d5dfe359743e539
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5786,6 +5786,10 @@ public final class Settings {
          * @hide
          */
         public static final String STATUSBAR_COLORED_ICONS = "statusbar_colored_icons";
+	/**
+         * @hide
+         */
+        public static final String LOCKSCREEN_WEATHER_ENABLED = "lockscreen_weather_enabled";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
